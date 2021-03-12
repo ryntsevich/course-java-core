0e4b24976c7d4975f5b2b72d758eaeda2f194329
ryntsevich <ryntsevich.oksana@gmail.com>
2021-03-12 16:13:55 +0300

    feat: task 4 for theme "Loops"

--- src/main/java/com/rakovets/course/javabasics/practice/loops/Task04.java
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task04.java
@@ -8,6 +8,8 @@ package com.rakovets.course.javabasics.practice.loops;
  * @author Dmitry Rakovets
  */
 class Task04 {
+    private static final int ONE_HUNDRED_PERCENT = 100;
+
     /**
      * The entry point of the task
      *
@@ -17,8 +19,8 @@ class Task04 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int healthPoints = 1000;
-        double regenerationPercentFromCurrentHealth = 100;
+        int healthPoints = 100;
+        double regenerationPercentFromCurrentHealth = 5.0;
         int hoursAfterRespawn = 10;
 
         int featureHealthPoint = calculateHealthPointsByTime(healthPoints, regenerationPercentFromCurrentHealth, hoursAfterRespawn);
@@ -34,8 +36,10 @@ class Task04 {
      * @return количество HP
      */
     static int calculateHealthPointsByTime(int healthPoints, double regenerationPercentFromCurrentHealth, int hoursAfterRespawn) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        double resultHealth = healthPoints;
+        for (int i = 1; i <= hoursAfterRespawn; i++) {
+            resultHealth += resultHealth * regenerationPercentFromCurrentHealth / ONE_HUNDRED_PERCENT;
+        }
+        return (int) resultHealth;
     }
 }
