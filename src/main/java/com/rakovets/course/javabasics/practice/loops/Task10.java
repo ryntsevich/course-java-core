5b7de7c5cc6595e1503bc57dcedba83d5265a5d8
ryntsevich <ryntsevich.oksana@gmail.com>
2021-03-16 10:33:48 +0300

    feat: task 10 for theme "Loops"

--- src/main/java/com/rakovets/course/javabasics/practice/loops/Task10.java
+++ src/main/java/com/rakovets/course/javabasics/practice/loops/Task10.java
@@ -14,6 +14,9 @@ package com.rakovets.course.javabasics.practice.loops;
  * @author Dmitry Rakovets
  */
 class Task10 {
+    private static final int ONE_NUMBER = 1;
+    private static final int TWO_NUMBER = 2;
+    private static final int ZERO_REMAINDER = 0;
     /**
      * The entry point of the task
      *
@@ -24,7 +27,7 @@ class Task10 {
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
         int startNumber = 0;
-        int finishNumber = 50;
+        int finishNumber = 9;
 
         int countPrimeNumber = countPrimeNumber(startNumber, finishNumber);
         System.out.printf("Result:\n%d", countPrimeNumber);
@@ -38,8 +41,27 @@ class Task10 {
      * @return количество простых чисел
      */
     static int countPrimeNumber(int startNumber, int finishNumber) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0;
+        int countPrimeNumbers = 0;
+        for (int i = startNumber; i <= finishNumber; i++) {
+            if (isPrime(i)) {
+                countPrimeNumbers++;
+            }
+        }
+        return countPrimeNumbers;
+    }
+
+    static boolean isPrime(int number) {
+        boolean isPrimeNumber = true;
+        if (number <= ONE_NUMBER) {
+            isPrimeNumber = false;
+        }
+        double limit = Math.sqrt(number);
+        for (int j = TWO_NUMBER; j <= limit; j++) {
+            if (number % j == ZERO_REMAINDER) {
+                isPrimeNumber = false;
+                break;
+            }
+        }
+        return isPrimeNumber;
     }
 }
