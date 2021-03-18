c66ed69fe220ed0c49f689f11c7f1c78986db6d9
ryntsevich <ryntsevich.oksana@gmail.com>
2021-03-18 16:01:06 +0300

    feat: method getAverageMark for Task 01 for theme "Array"

--- src/main/java/com/rakovets/course/java/core/practice/array/Task01.java
+++ src/main/java/com/rakovets/course/java/core/practice/array/Task01.java
@@ -1,5 +1,8 @@
 package com.rakovets.course.java.core.practice.array;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Разработать программу для электронного дневника, которая работает с отметками только по одному предмету.
  *
@@ -32,9 +35,11 @@ class Task01 {
      * @return средняя арифметическая отметка
      */
     static double getAverageMark(int[] marks) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return 0.0;
+        int sum = 0;
+        for (int i = 0; i < marks.length; i++) {
+            sum += marks[i];
+        }
+        return BigDecimal.valueOf((double) sum / marks.length).setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
 
     /**
