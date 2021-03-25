eecd1390f3a7b6cfd57bd054a84a8b0245b7478c
ryntsevich <ryntsevich.oksana@gmail.com>
2021-03-25 17:17:39 +0300

    feat: add task Point for theme "OOP. Classes and Objects"

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop/classesandobjects/Point.java
@@ -0,0 +1,31 @@
+package com.rakovets.course.java.core.practice.oop.classesandobjects;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double getDistance(Point newPoint) {
+        return Math.sqrt(Math.pow((newPoint.x - x), 2) + Math.pow((newPoint.y - y), 2));
+    }
+}
