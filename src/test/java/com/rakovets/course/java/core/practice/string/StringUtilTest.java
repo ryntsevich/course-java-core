6b79fc1d8a2c8ca3114e1dd2f14f722deaa1cfc0
ryntsevich <ryntsevich.oksana@gmail.com>
2021-04-08 12:38:45 +0300

    test: Test for method getIndexForChar

--- src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
@@ -1,6 +1,8 @@
 package com.rakovets.course.java.core.practice.string;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -20,6 +22,21 @@ public class StringUtilTest {
     @MethodSource("concatProviderArguments")
     void getConcatString(String expected, String str1, String str2) {
         String actual = StringUtil.getConcatString(str1, str2);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public static Stream<Arguments> getIndexProviderArguments() {
+        return Stream.of(
+                Arguments.of(1, "HelloJava", 'e'),
+                Arguments.of(-1, "HelloJava", 'k'),
+                Arguments.of(-1, "HelloJava", '0')
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getIndexProviderArguments")
+    void getIndexForChar(int expected, String str, char symbol) {
+        int actual = StringUtil.getIndexForChar(str, symbol);
         Assertions.assertEquals(expected, actual);
     }
 }
