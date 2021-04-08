afc22d3b8fac30cf26e5d6ca3a542274e5fce920
ryntsevich <ryntsevich.oksana@gmail.com>
2021-04-08 14:22:15 +0300

    feat: method isEquals for Task3

--- src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
@@ -7,4 +7,7 @@ public class StringUtil {
     public static int getIndexForChar(String str, char symbol) {
         return str.indexOf(symbol);
     }
+    public static boolean isEquals(String str1, String str2) {
+        return str1.equals(str2);
+    }
 }
