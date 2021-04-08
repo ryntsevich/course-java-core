74697329fd0c2f50dd208d7d3fbf181e938be938
ryntsevich <ryntsevich.oksana@gmail.com>
2021-04-08 12:14:59 +0300

    test: add Test for method getConcatString

---
+++ src/test/java/com/rakovets/course/java/core/practice/string/StringUtilTest.java
@@ -0,0 +1,25 @@
+package com.rakovets.course.java.core.practice.string;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class StringUtilTest {
+    public static Stream<Arguments> concatProviderArguments() {
+        return Stream.of(
+                Arguments.of("HelloJava", "Hello", "Java"),
+                Arguments.of("Java", "", "Java"),
+                Arguments.of("Hello", "Hello", "")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("concatProviderArguments")
+    void getConcatString(String expected, String str1, String str2) {
+        String actual = StringUtil.getConcatString(str1, str2);
+        Assertions.assertEquals(expected, actual);
+    }
+}
