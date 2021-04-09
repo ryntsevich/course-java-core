9675b5d375bb53c2da098ee4ba6855c0be19ca1d
ryntsevich <ryntsevich.oksana@gmail.com>
2021-04-09 10:23:13 +0300

    feat: method replaceEmoji for Task6

--- src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
+++ src/main/java/com/rakovets/course/java/core/practice/string/StringUtil.java
@@ -4,16 +4,26 @@ public class StringUtil {
     public static String getConcatString(String str1, String str2) {
         return str1.concat(str2);
     }
+
     public static int getIndexForChar(String str, char symbol) {
         return str.indexOf(symbol);
     }
+
     public static boolean isEquals(String str1, String str2) {
         return str1.equals(str2);
     }
+
     public static String getTrimAndUpperCase(String str) {
         return str.trim().toUpperCase();
     }
+
     public static String getSubstring(String str, int beginIndex, int endIndex) {
         return str.substring(beginIndex, endIndex);
     }
+
+    public static String replaceEmoji(String str) {
+        return str.replace(":(", ":)");
+    }
+
+
 }
