36f23935ce49db5bb0e1e4959e2ab83c7856149f
ryntsevich <ryntsevich.oksana@gmail.com>
2021-04-15 11:08:09 +0300

    feat: add Demo for method compareSpeedOfAdditionMethod

---
+++ src/test/java/com/rakovets/course/java/core/practice/string/CompareSpeedOfAdditionMethod.java
@@ -0,0 +1,10 @@
+package com.rakovets.course.java.core.practice.string;
+
+public class CompareSpeedOfAdditionMethod {
+    public static void main(String[] args) {
+        System.out.println(StringUtil.compareSpeedOfAdditionMethod(10));
+        System.out.println(StringUtil.compareSpeedOfAdditionMethod(100));
+        System.out.println(StringUtil.compareSpeedOfAdditionMethod(1000));
+        System.out.println(StringUtil.compareSpeedOfAdditionMethod(10000));
+    }
+}
