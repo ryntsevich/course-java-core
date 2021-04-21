06bcd12062d330f69a9af831838e6fc57196169f
ryntsevich <ryntsevich.oksana@gmail.com>
2021-04-21 12:04:37 +0300

    feat: method mew, purr with Person for class Persian for Task5

--- src/main/java/com/rakovets/course/java/core/practice/oop/principles/catshome/Persian.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/principles/catshome/Persian.java
@@ -1,6 +1,9 @@
 package com.rakovets.course.java.core.practice.oop.principles.catshome;
 
 public class Persian extends Cat {
+    private final double UP_HAPPINESS = 15.0;
+    private final double DOWN_HAPPINESS = -15.0;
+
     public Persian(String name) {
         super(name);
     }
@@ -13,5 +16,15 @@ public class Persian extends Cat {
     @Override
     public String purr() {
         return "purr-persian";
+    }
+
+    @Override
+    public void mew(Person user) {
+        user.changeHappiness(DOWN_HAPPINESS);
+    }
+
+    @Override
+    public void purr(Person user) {
+        user.changeHappiness(UP_HAPPINESS);
     }
 }
