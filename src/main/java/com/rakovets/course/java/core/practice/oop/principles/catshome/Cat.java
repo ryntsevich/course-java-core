dc98cadab1c497f9eccd459d1ae309d62eb4de27
ryntsevich <ryntsevich.oksana@gmail.com>
2021-04-21 11:48:48 +0300

    feat: method mew, purr with Person for class Cat for Task4

--- src/main/java/com/rakovets/course/java/core/practice/oop/principles/catshome/Cat.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/principles/catshome/Cat.java
@@ -1,17 +1,11 @@
 package com.rakovets.course.java.core.practice.oop.principles.catshome;
 
 public class Cat {
+    private final double UP_HAPPINESS = 10.0;
+    private final double DOWN_HAPPINESS = -10.0;
     private String name;
 
     public Cat(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
 
@@ -22,4 +16,22 @@ public class Cat {
     public String purr() {
         return "purr";
     }
+
+    public void mew(Person user) {
+        user.changeHappiness(DOWN_HAPPINESS);
+    }
+
+    public void purr(Person user) {
+        user.changeHappiness(UP_HAPPINESS);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
