fd476cecd69eea4daddee75bcaf148c1b944d679
ryntsevich <ryntsevich.oksana@gmail.com>
2021-04-21 12:05:14 +0300

    test: Test for methods mew, purr with Person for class Persian

--- src/test/java/com/rakovets/course/java/core/practice/oop/principles/catshome/PersianTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop/principles/catshome/PersianTest.java
@@ -3,6 +3,11 @@ package com.rakovets.course.java.core.practice.oop.principles.catshome;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,6 +30,39 @@ class PersianTest {
 
         String actual = new Persian("Tom").purr();
 
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public static Stream<Arguments> mewWithPersianProviderArguments() {
+        return Stream.of(
+                Arguments.of(5.0, new Person(20.0)),
+                Arguments.of(0.0, new Person(15.0)),
+                Arguments.of(-35.0, new Person(-20.0))
+        );
+    }
+
+    @ParameterizedTest(name = "Method \"mew\" for Persian with Person")
+    @MethodSource("mewWithPersianProviderArguments")
+    void mewWithPersian(double expected, Person user) {
+        Persian cat = new Persian("Tom");
+        cat.mew(user);
+        double actual = user.getHappiness();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public static Stream<Arguments> purrWithPersianProviderArguments() {
+        return Stream.of(
+                Arguments.of(35.0, new Person(20.0)),
+                Arguments.of(-5.0, new Person(-20.0))
+        );
+    }
+
+    @ParameterizedTest(name = "Method \"purr\" for Persian with Person")
+    @MethodSource("purrWithPersianProviderArguments")
+    void purrWithPersian(double expected, Person user) {
+        Persian cat = new Persian("Tom");
+        cat.purr(user);
+        double actual = user.getHappiness();
         Assertions.assertEquals(expected, actual);
     }
 }
