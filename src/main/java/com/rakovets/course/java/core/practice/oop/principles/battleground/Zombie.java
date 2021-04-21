3c78718d44fa37f40df5a1afd7b192c814e19a58
ryntsevich <ryntsevich.oksana@gmail.com>
2021-04-21 17:32:31 +0300

    feat: superPower for Zombie for Task6

--- src/main/java/com/rakovets/course/java/core/practice/oop/principles/battleground/Zombie.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/principles/battleground/Zombie.java
@@ -1,14 +1,28 @@
 package com.rakovets.course.java.core.practice.oop.principles.battleground;
 
-public class Zombie extends Enemy{
-    private final int DAMAGE = 20;
+import java.util.Random;
+
+public class Zombie extends Enemy {
+    private final int DAMAGE = 200;
+    public int healthZombie;
+
 
     public Zombie(int health) {
         super(health);
+        this.healthZombie = health;
     }
 
     @Override
     public void attackHero(Hero man) {
         man.takeDamage(DAMAGE);
+    }
+
+    public void superPower(Zombie enemy) {
+        if (enemy.getHealth() <= 0) {
+            Random value = new Random();
+            if (value.nextBoolean()) {
+                enemy.setHealth(healthZombie);
+            }
+        }
     }
 }
