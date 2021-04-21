5374ed79a30b1a5d1b6bcc04dd9a70e16dac2d70
ryntsevich <ryntsevich.oksana@gmail.com>
2021-04-21 12:15:46 +0300

    feat: methods mew, purr with Person for class Sphynx for Task5

--- src/main/java/com/rakovets/course/java/core/practice/oop/principles/catshome/Sphynx.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/principles/catshome/Sphynx.java
@@ -1,6 +1,9 @@
 package com.rakovets.course.java.core.practice.oop.principles.catshome;
 
 public class Sphynx extends Cat {
+    private final double UP_HAPPINESS = 30.0;
+    private final double DOWN_HAPPINESS = -30.0;
+
     public Sphynx(String name) {
         super(name);
     }
@@ -13,6 +16,16 @@ public class Sphynx extends Cat {
     @Override
     public String purr() {
         return "purr-sphynx";
+    }
+
+    @Override
+    public void mew(Person user) {
+        user.changeHappiness(DOWN_HAPPINESS);
+    }
+
+    @Override
+    public void purr(Person user) {
+        user.changeHappiness(UP_HAPPINESS);
     }
 
 }
