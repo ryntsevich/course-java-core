85d0b745b01d120504d6081276a814c8eca3c912
ryntsevich <ryntsevich.oksana@gmail.com>
2021-04-21 12:09:43 +0300

    feat: methods mew, purr with Person for class Siamese for Task5

--- src/main/java/com/rakovets/course/java/core/practice/oop/principles/catshome/Siamese.java
+++ src/main/java/com/rakovets/course/java/core/practice/oop/principles/catshome/Siamese.java
@@ -1,6 +1,8 @@
 package com.rakovets.course.java.core.practice.oop.principles.catshome;
 
 public class Siamese extends Cat {
+    private final double UP_HAPPINESS = 20.0;
+    private final double DOWN_HAPPINESS = -20.0;
     public Siamese(String name) {
         super(name);
     }
@@ -13,5 +15,15 @@ public class Siamese extends Cat {
     @Override
     public String purr() {
         return "purr-siamese";
+    }
+
+    @Override
+    public void mew(Person user) {
+        user.changeHappiness(DOWN_HAPPINESS);
+    }
+
+    @Override
+    public void purr(Person user) {
+        user.changeHappiness(UP_HAPPINESS);
     }
 }
