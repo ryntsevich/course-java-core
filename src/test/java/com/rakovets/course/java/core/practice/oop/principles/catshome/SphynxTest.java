21aada5ec299338f6cb247d285aacef0547ecd75
ryntsevich <ryntsevich.oksana@gmail.com>
2021-04-21 12:16:05 +0300

    test: Test for methods mew, purr with Person for class Sphynx

--- src/test/java/com/rakovets/course/java/core/practice/oop/principles/catshome/SphynxTest.java
+++ src/test/java/com/rakovets/course/java/core/practice/oop/principles/catshome/SphynxTest.java
@@ -3,8 +3,11 @@ package com.rakovets.course.java.core.practice.oop.principles.catshome;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
 
 class SphynxTest {
 
@@ -25,6 +28,39 @@ class SphynxTest {
 
         String actual = new Sphynx("Tom").purr();
 
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public static Stream<Arguments> mewWithSphynxProviderArguments() {
+        return Stream.of(
+                Arguments.of(10.0, new Person(40.0)),
+                Arguments.of(0.0, new Person(30.0)),
+                Arguments.of(-50.0, new Person(-20.0))
+        );
+    }
+
+    @ParameterizedTest(name = "Method \"mew\" for Sphynx with Person")
+    @MethodSource("mewWithSphynxProviderArguments")
+    void mewWithSphynx(double expected, Person user) {
+        Sphynx cat = new Sphynx("Tom");
+        cat.mew(user);
+        double actual = user.getHappiness();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    public static Stream<Arguments> purrWithSphynxProviderArguments() {
+        return Stream.of(
+                Arguments.of(50.0, new Person(20.0)),
+                Arguments.of(0.0, new Person(-30.0))
+        );
+    }
+
+    @ParameterizedTest(name = "Method \"purr\" for Sphynx with Person")
+    @MethodSource("purrWithSphynxProviderArguments")
+    void purrWithSphynx(double expected, Person user) {
+        Sphynx cat = new Sphynx("Tom");
+        cat.purr(user);
+        double actual = user.getHappiness();
         Assertions.assertEquals(expected, actual);
     }
 }
