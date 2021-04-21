f90b791353eba70468e60b52dc7c0ab33a978a9f
ryntsevich <ryntsevich.oksana@gmail.com>
2021-04-21 14:52:23 +0300

    feat: class Enemy for Task3

---
+++ src/main/java/com/rakovets/course/java/core/practice/oop/principles/battleground/Enemy.java
@@ -0,0 +1,21 @@
+package com.rakovets.course.java.core.practice.oop.principles.battleground;
+
+public class Enemy {
+    private int health;
+
+    public Enemy(int health) {
+        this.health = health;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void takeDamage(int damage) {
+        health -= damage;
+    }
+}
